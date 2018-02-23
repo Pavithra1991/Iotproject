4f7f25b7b27037f7a22c3d5f01178166814777c7
Pavithra1991 <smile.pavi3@gmail.com>
2018-02-23 13:16:52 +0530

    Iot Project

---
+++ iotproject/src/iotproject/Iot.java
@@ -0,0 +1,10 @@
+package iotproject;
+
+public class Iot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welocme to Internet Of Things");
+	}
+
+}
